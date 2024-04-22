3af405f36df6b0a6135c3de13761f2a447b4b5e0
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-22 18:08:59 +0200

    send signal

--- core/send_signal.c
+++ core/send_signal.c
@@ -3,6 +3,8 @@
 /**
  * send_signal - send a signal to a specific process or a group of processes
  *
+ * @pid: the ID of the process
+ * @signal: represents the signal that will be sent
  * Return: 1 if success otherwise 0
  */
 
@@ -10,11 +12,11 @@ int send_signal(pid_t pid, int signal)
 {
 	if (!kill(pid, signal))
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
 		perror("Error sending signal");
-		return 0;
+		return (0);
 	}
 }
