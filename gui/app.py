b8ebffbf2e902762749ad5e220fba9600e83e255
LORDyyyyy <mlord6271@gmail.com>
2024-04-26 21:16:19 +0300

    finished table view of the processes list

--- gui/app.py
+++ gui/app.py
@@ -1,11 +1,14 @@
 import tkinter as tk
-from typing import Dict
+from typing import Dict, List, Union
 
 import ttkbootstrap as tb
 from call_c_functions import call_list_process, call_send_signal
+from library import SIGNALS
 from ttkbootstrap.dialogs import Messagebox
+from ttkbootstrap.tableview import Tableview
 
 PROCESS_TYPE = {'User': 0, 'All': 1}
+
 
 class ProcessManager():
 
@@ -14,62 +17,125 @@ class ProcessManager():
         self.root.title('Process Manager')
         self.root.geometry('1300x700')
 
-        self.pro_type_menu = tb.Menubutton(self.root,
-                                           text="Type",
+        self.colors = self.root.style.colors
+
+        self.header_frame = tb.Frame(self.root)
+        self.header_frame.pack(padx=10, fill=tk.X)
+
+        self.table_frame = tb.Frame(self.root, bootstyle='info')
+        self.table_frame.pack(fill=tk.BOTH, expand=True, padx=10)
+
+        self.pro_type_var = tk.StringVar()
+        self.pro_type_var.set('User')
+
+        self.pro_refrech_button = tb.Button(self.header_frame,
+                                            text="Refresh",
+                                            command=lambda: self.update_table_date(),
+                                            bootstyle="info")
+
+        self.pro_type_menu = tb.Menubutton(self.header_frame,
+                                           text='Type',
+                                           textvariable=self.pro_type_var,
                                            bootstyle="info")
         self.pro_type_insied_menu = tb.Menu(self.pro_type_menu)
 
-        self.pro_type_var = tk.StringVar()
         for index, x in enumerate(['User', 'All']):
             self.pro_type_insied_menu.add_radiobutton(label=x,
                                                       variable=self.pro_type_var,
-                                                      font=('Arial', 14),
-                                                      command=lambda i=index: self.update_process_type(i))
+                                                      command=lambda: self.update_table_date(),
+                                                      font=('Arial', 14),)
         self.pro_type_menu['menu'] = self.pro_type_insied_menu
 
         self.pro_type_label = tb.Label(
-            self.root, text="Process Type",
-            bootstyle="info", font=('Arial', 18))
+            self.header_frame, text='Process Type',
+            bootstyle="info", font=('', 18))
 
-        self.pro_type_label.grid(row=0, column=0, padx=10, pady=10)
-        self.pro_type_menu.grid(row=0, column=1, padx=10, pady=10)
+        self.pro_type_label.pack(side=tk.LEFT, padx=10, pady=10)
+        self.pro_type_menu.pack(side=tk.LEFT, padx=10, pady=10)
+        self.pro_refrech_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-    def update_process_type(self, type: int) -> None:
+        self.do_treeview()
+        self.update_table_date()
+
+        self.do_signal()
+
+    def update_process_type(self, type: int) -> Union[List, None]:
+        """Get a new version of the process list based on the type
+
+        Args:
+            type: the type of process to list
+
+        Returns:
+            List: a list of processes or None if an error occurred
+        """
         self.pro_type_menu.config(text=self.pro_type_var.get())
         process_list = call_list_process(type)
         if 'Error' in process_list.keys():
             Messagebox.showerror('Error', process_list['Error'])
             return
-        self.do_treeview(process_list)
-        print(process_list)
-        print(self.pro_type_var.get())
+        # print(process_list)
+        # print(self.pro_type_var.get())
+        return process_list
 
-    def do_treeview(self, process_list: Dict[str, str]) -> None:
-        columns = ['USER', 'PID', '%CPU', '%MEM', 'TIME', 'COMMAND']
-        self.tree = tb.Treeview(self.root,
-                                bootstyle="success",
-                                columns=columns,
-                                show="headings"
-                                )
-        for col in columns:
-            self.tree.heading(col, text=col)
+    def do_treeview(self) -> None:
+        """Create the tableview widget to display the process list
+        """
+        process_list = self.update_process_type(
+            PROCESS_TYPE[self.pro_type_var.get()])
 
-        for index, process in enumerate(process_list['process']):
-            self.tree.insert('', index, values=process)
+        if process_list is None:
+            return
+        self.columns = ['USER', 'PID', '%CPU', '%MEM', 'TIME', 'COMMAND']
+        self.columns = [{'text': x, "stretch": False} for x in self.columns]
 
-        self.tree.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
-        h_scroll = tb.Scrollbar(self.root, orient='horizontal')
-        v_scroll = tb.Scrollbar(self.root, orient='vertical')
-        self.configure_tree_scrollbars(self.tree, h_scroll, v_scroll)
-        h_scroll.grid(row=2, column=0, columnspan=2, sticky='ew')
-        v_scroll.grid(row=1, column=2, sticky='ns')
+        self.table = Tableview(self.table_frame,
+                               coldata=self.columns,
+                               rowdata=process_list['process'],
+                               searchable=True,
+                               stripecolor=(self.colors.light, None),
+                               autofit=True,
+                               autoalign=True,
+                               height=10
+                               )
+        self.table.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
-        self.tree.after(1000, lambda: self.update_process_type(PROCESS_TYPE[self.pro_type_var.get()]))
+    def update_table_date(self) -> None:
+        """Update the table data
+        based on the process type on change of the process type
+        or refresh button click
+        """
+        process_list = self.update_process_type(
+            PROCESS_TYPE[self.pro_type_var.get()])
+        if process_list is None:
+            return
+        self.table.build_table_data(
+            coldata=self.columns, rowdata=process_list['process'])
 
+    def do_signal(self):
+        # PID input Row
+        third_row_frame = tk.Frame(self.root)
+        third_row_frame.pack(pady=10)
 
-    @staticmethod
-    def configure_tree_scrollbars(tree, h_scroll, v_scroll):
-        tree.configure(xscrollcommand=h_scroll.set,
-                       yscrollcommand=v_scroll.set)
-        h_scroll.config(command=tree.xview)
-        v_scroll.config(command=tree.yview)
+        tk.Label(third_row_frame, text="Enter PID:").pack(side=tk.LEFT)
+        self.pid_entry = tk.Entry(third_row_frame)
+        self.pid_entry.pack(side=tk.LEFT)
+
+        # Signal Type Dropdown Row
+        fourth_row_frame = tk.Frame(self.root)
+        fourth_row_frame.pack(pady=10)
+
+        tk.Label(fourth_row_frame, text="Signal Type:").pack(side=tk.LEFT)
+        self.signal_type = tk.StringVar()
+        self.signal_type.set("SIGKILL")
+        signal_dropdown = tb.Combobox(
+            fourth_row_frame, textvariable=self.signal_type)
+        signal_dropdown['values'] = tuple(SIGNALS.keys())
+        signal_dropdown.pack(side=tk.LEFT)
+
+        # Send Signal Button Row
+        fifth_row_frame = tk.Frame(self.root)
+        fifth_row_frame.pack(pady=10)
+
+        send_button = tk.Button(
+            fifth_row_frame, text="Send Signal")
+        send_button.pack()
--- gui/call_c_functions.py
+++ gui/call_c_functions.py
@@ -3,7 +3,17 @@ from typing import Dict, List, Union
 from library import SIGNALS, free_2d_array, list_processes, send_signal
 
 
-def call_list_process(type: int) -> Dict[str, Union[str, List[str]]]:
+def call_list_process(type: int) -> Dict:
+    """Call the list_processes function from the C shared library
+
+    Args:
+        type (int): the  type of process to list
+
+
+    Returns:
+        Dict: a dictionary containing the list of processes
+            or an error message if an error occurred
+    """
     if type not in [0, 1]:
         return {'Error': 'Invalid process type'}
 
@@ -19,17 +29,44 @@ def call_list_process(type: int) -> Dict[str, Union[str, List[str]]]:
         splitted_list = process.decode('utf-8').split()
         if len(splitted_list) < 5:
             continue
+
         formatted_processes.append(splitted_list[:5])
+
+        try:
+            if counter != 0:
+                formatted_processes[counter][1] = int(
+                    formatted_processes[counter][1])
+
+                formatted_processes[counter][2] = float(
+                    formatted_processes[counter][2])
+
+                formatted_processes[counter][3] = float(
+                    formatted_processes[counter][3])
+        except ValueError:
+            continue
+
         formatted_processes[counter].append(
             ' '.join(splitted_list[5:]))
         counter += 1
 
     free_2d_array(processes)
 
-    return {'process': formatted_processes}
+    return {'process': formatted_processes[1:]}
 
 
 def call_send_signal(pid: int, signal: int) -> Dict[str, str]:
+    """Call the send_signal function from the C shared library
+
+    Args:
+        pid: Process ID to send the signal to
+        signal: Signal to send to the process.
+                must be one of the signals in the SIGNALS dictionary
+
+    Returns:
+        Dict: a dictionary containing a success message
+            if the signal was sent successfully
+            or an error message if an error occurred
+    """
     if pid <= 0:
         return {'Error': 'Invalid PID'}
 
